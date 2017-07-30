fa30d9eef0de9500a89487a7dd1865d18833f6d7
Taymison <taymisondeveloper@gmail.com>
2017-07-30 10:33:18 -0300

    Writing Javascript for Our Header Menu lesson

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-const $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-
-    payTaxes() {
-        console.log(this.name + " now ownes $0 in taxes.");
-    }
-}
-
-const john = new Person("John Doe", "blue");
-john.greet();
-
-const jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+const mobileMenu = new MobileMenu();
